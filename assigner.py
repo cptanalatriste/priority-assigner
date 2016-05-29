7fe02ac4b7b3258fda0e0b750d9134b1e1673508
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-05-29 10:03:04 +0100

    Trying with KNN

--- assigner.py
+++ assigner.py
@@ -3,19 +3,24 @@ This module builds a predictor for the priority field for a bug report
 """
 
 import pandas as pd
+
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
+from sklearn.neighbors import KNeighborsClassifier
 
-CSV_FILE = "C:\Users\Carlos G. Gavidia\git\priority-assigner\sample_file.csv"
+from sklearn.metrics import classification_report
+
+import matplotlib.pyplot as plt
+
+import fselect
+
+CSV_FILE = "C:\Users\Carlos G. Gavidia\git\github-data-miner\Release_Counter_.csv"
 
 
 def get_issues_dataframe(filename):
     """
-    Returns a dataframe of issues that: Are resolived, have a commit in Git, were not resolved by the reported and had
+    Returns a dataframe of issues that: Are resolved, have a commit in Git, were not resolved by the reported and had
     a priority change not made by the reporter.
 
     Also, that does not have the distance in releases null.
@@ -58,12 +63,16 @@ def encode_nominal_features(issue_dataframe, nominal_features):
     :param nominal_features: List of nominal features.
     :return: New dataframe.
     """
-    dummy_dataframe = pd.get_dummies(issue_dataframe[nominal_features])
 
-    temp_dataframe = pd.concat([issue_dataframe, dummy_dataframe], axis=1)
-    temp_dataframe = temp_dataframe.drop(nominal_features, axis=1)
+    if nominal_features:
+        dummy_dataframe = pd.get_dummies(issue_dataframe[nominal_features])
+        temp_dataframe = pd.concat([issue_dataframe, dummy_dataframe], axis=1)
+        temp_dataframe = temp_dataframe.drop(nominal_features, axis=1)
 
-    return temp_dataframe
+        return temp_dataframe
+
+    else:
+        return issue_dataframe
 
 
 def escale_features(issues_train, issues_test, numerical_features):
@@ -87,6 +96,28 @@ def escale_features(issues_train, issues_test, numerical_features):
     return issues_train_std, issues_test_std
 
 
+def evaluate_performance(prefix, classifier, issues_train_std, priority_train, issues_test_std, priority_test):
+    """
+    Calculates performance metrics for a classifier.
+    :param prefix: A prefix, for identifying the classifier.
+    :param classifier: The classifier.
+    :param issues_train_std: Train features.
+    :param priority_train: Train class.
+    :param issues_test_std: Test features.
+    :param priority_test: Test class.
+    :return: None.
+    """
+    print prefix, ': Training accuracy ', classifier.score(issues_train_std, priority_train)
+    train_predictions = classifier.predict(issues_train_std)
+    print prefix, " :TRAIN DATA SET"
+    print classification_report(y_true=priority_train, y_pred=train_predictions)
+
+    print prefix, ': Test accuracy ', classifier.score(issues_test_std, priority_test)
+    test_predictions = classifier.predict(issues_test_std)
+    print prefix, " :TEST DATA SET"
+    print classification_report(y_true=priority_test, y_pred=test_predictions)
+
+
 def select_features_l1(issues_train_std, priority_train, issues_test_std, priority_test):
     """
     Applies a Logistic Regression using L1-regularization, in order to get a sparse solution.
@@ -99,30 +130,64 @@ def select_features_l1(issues_train_std, priority_train, issues_test_std, priori
     logistic_regression = LogisticRegression(penalty='l1', C=0.1)
     logistic_regression.fit(issues_train_std, priority_train)
 
-    priority_predictions = logistic_regression.predict(issues_test_std)
+    # print 'Intercept: ', logistic_regression.intercept_
+    # print 'Coefficient: ', logistic_regression.coef_
 
-    print 'Training accuracy ', logistic_regression.score(issues_train_std, priority_train)
-    print 'Test accuracy ', logistic_regression.score(issues_test_std, priority_test)
+    evaluate_performance("LOGIT-L1", logistic_regression, issues_train_std, priority_train, issues_test_std,
+                         priority_test)
 
-    print 'Intercept: ', logistic_regression.intercept_
-    print 'Coefficient: ', logistic_regression.coef_
 
-    print 'Test Precission: ', precision_score(y_true=priority_test, y_pred=priority_predictions)
-    print 'Test Recall: ', recall_score(y_true=priority_test, y_pred=priority_predictions)
-    print 'Test F1: ', f1_score(y_true=priority_test, y_pred=priority_predictions)
+def sequential_feature_selection(issues_train_std, priority_train, issues_test_std, priority_test):
+    """
+    Applies a sequential feature selection algorithm and evaluates its performance using a k-neighbors classifier (5 neighbors).
+    :param issues_train_std:
+    :param priority_train:
+    :param issues_test_std:
+    :param priority_test:
+    :return:
+    """
 
-    train_predictions = logistic_regression.predict(issues_train_std)
-    print 'Train Precission: ', precision_score(y_true=priority_train, y_pred=train_predictions)
-    print 'Train Recall: ', recall_score(y_true=priority_train, y_pred=train_predictions)
-    print 'Train F1: ', f1_score(y_true=priority_train, y_pred=train_predictions)
+    # TODO: What is the optimal number of neighbors?
+    knn_classifier = KNeighborsClassifier(n_neighbors=5)
+    feature_selector = fselect.SBS(knn_classifier, k_features=1)
+    feature_selector.fit(issues_train_std.values, priority_train.values)
+
+    optimal_subset = None
+    for subset in feature_selector.subsets_:
+
+        if len(subset) == 5:
+            print "Number of features: ", len(subset)
+            print issues_train_std.columns[list(subset)]
+            optimal_subset = list(subset)
+
+    num_features = [len(k) for k in feature_selector.subsets_]
+    plt.plot(num_features, feature_selector.scores_, marker='o')
+    plt.ylim([0.0, 1.1])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Number of features')
+    plt.grid()
+
+    # Uncomment to display plot.
+    plt.show()
+
+    knn_classifier.fit(issues_train_std, priority_train)
+    evaluate_performance("KNN-5NEIGH", knn_classifier, issues_train_std, priority_train, issues_test_std, priority_test)
+
+    new_train = issues_train_std.iloc[:, optimal_subset]
+    new_test = issues_test_std.iloc[:, optimal_subset]
+
+    knn_classifier.fit(new_train, priority_train)
+    evaluate_performance("KNN-OPT", knn_classifier, new_train, priority_train, new_test, priority_test)
 
 
 if __name__ == "__main__":
     issue_dataframe = get_issues_dataframe(CSV_FILE)
 
     class_label = 'Encoded Priority'
-    numerical_features = ['Commits', 'GitHub Distance in Releases', 'Total LOC', 'Git Resolution Time',
-                          'Comments in JIRA']
+    numerical_features = ['Commits', 'GitHub Distance in Releases', 'Avg Lines', 'Git Resolution Time',
+                          'Comments in JIRA', 'Total Deletions', 'Total Insertions', 'Avg Files', 'Change Log Size',
+                          'Number of Reopens']
+
     nominal_features = ['Git Repository']
 
     issue_dataframe = encode_class_label(issue_dataframe, class_label)
@@ -132,12 +197,15 @@ if __name__ == "__main__":
     encoded_priorities = issue_dataframe[class_label]
     issue_dataframe = issue_dataframe.drop([class_label], axis=1)
 
+    print "Number of features: ", len(issue_dataframe.columns)
+
     issues_train, issues_test, priority_train, priority_test = train_test_split(issue_dataframe,
                                                                                 encoded_priorities,
-                                                                                test_size=0.3, random_state=0)
+                                                                                test_size=0.2, random_state=0)
 
     print len(issues_train.index), " issues on the train set."
 
     issues_train_std, issues_test_std = escale_features(issues_train, issues_test, numerical_features)
 
     select_features_l1(issues_train_std, priority_train, issues_test_std, priority_test)
+    sequential_feature_selection(issues_train_std, priority_train, issues_test_std, priority_test)
