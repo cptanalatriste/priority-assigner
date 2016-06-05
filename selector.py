aedeb90c07553e278126bace5b3aa883de76ab78
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-06-05 18:15:07 +0100

    Now the test-train split is done through stratified sampling

--- selector.py
+++ selector.py
@@ -5,6 +5,7 @@ This module performs experiments to select a classification algorithm.
 import pandas as pd
 import numpy as np
 import traceback
+import winsound
 
 import assigner
 import tuning
@@ -15,6 +16,7 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 SCORING = 'f1_macro'
 
@@ -49,12 +51,29 @@ def get_algorithms():
                                               'kernel': ['rbf']}],
                                  scoring=SCORING,
                                  cv=cv,
-                                 n_jobs=n_jobs))]
+                                 n_jobs=n_jobs)),
+            ("RandomForest", GridSearchCV(estimator=RandomForestClassifier(random_state=0,
+                                                                           n_jobs=-1),
+                                          param_grid=[{'n_estimators': np.arange(1, 100, 10)}],
+                                          scoring=SCORING,
+                                          cv=cv,
+                                          n_jobs=n_jobs))]
 
 
 def analyse_performance(grid_search=None, algorithm=None, issues_train_std=None, priority_train=None,
                         issues_test_std=None,
                         priority_test=None):
+    """
+    Returns performance metrics for a classification algorithm, after tuning its parameters through grid search.
+
+    :param grid_search: Grid Search with possible configurations.
+    :param algorithm: Algorithm description.
+    :param issues_train_std: Issues in train.
+    :param priority_train: Priorities in train.
+    :param issues_test_std: Issues in test.
+    :param priority_test: Priorities in test.
+    :return: A tuple with performance metrics.
+    """
     try:
         mean_cv, std_cv = tuning.nested_cross_validation(grid_search, issues_train_std, priority_train,
                                                          SCORING)
@@ -80,6 +99,57 @@ def analyse_performance(grid_search=None, algorithm=None, issues_train_std=None,
         print trace
 
     return None
+
+
+def split_train_test(repository, issues, priorities):
+    """
+    Performs the train-test split using stratified sampling, and also normalized the numerical features.
+
+    :param repository: Repository name.
+    :param issues: Issues
+    :param priorities: Priorities.
+    :return: Normalized train issues, train priorities, normalized test issues, test priorities.
+    """
+    try:
+        issues_train, issues_test, priority_train, priority_test = train_test_split(issues, priorities,
+                                                                                    test_size=0.2,
+                                                                                    stratify=priorities,
+                                                                                    random_state=0)
+
+        issues_train_std, issues_test_std = assigner.escale_numerical_features(assigner.NUMERICAL_FEATURES,
+                                                                               issues_train,
+                                                                               issues_test)
+
+        return issues_train_std, priority_train, issues_test_std, priority_test
+
+    except ValueError as e:
+        print "!!!!!!  An error ocurred while splitting ", repository
+        trace = traceback.print_exc()
+        print trace
+
+    return None
+
+
+def write_results(results):
+    """
+    Writes the experiment results into a CSV file.
+    :param results: List with tuples, each containing experiment execution.
+    :return: None
+    """
+
+    file_name = "experiment_results.csv"
+
+    print "Writing results to ", file_name
+    results_dataframe = pd.DataFrame(data=results,
+                                     columns=["Algorithm", "CV Mean", " CV STD",
+                                              "Best configuration",
+                                              "Train accuracy", "Test accuracy", " Test f1-score",
+                                              "Test f1-score Pri 1", "Test f1-score Pri 2", "Test f1-score Pri 3",
+                                              "Test f1-score Pri 4", "Test f1-score Pri 5", "Test Support Pri 1",
+                                              "Test Support Pri 2", "Test Support Pri 3",
+                                              "Test Support Pri 4", "Test Support Pri 5", "Repository", "Total Issues"])
+
+    results_dataframe.to_csv(file_name, index=False)
 
 
 def main():
@@ -111,36 +181,23 @@ def main():
             issues, priorities = assigner.prepare_for_training(project_dataframe, assigner.CLASS_LABEL,
                                                                assigner.NUMERICAL_FEATURES, nominal_features)
 
-            issues_train, issues_test, priority_train, priority_test = train_test_split(issues, priorities,
-                                                                                        test_size=0.2, random_state=0)
+            train_test = split_train_test(repository, issues, priorities)
+            if train_test:
+                issues_train_std, priority_train, issues_test_std, priority_test = train_test
 
-            issues_train_std, issues_test_std = assigner.escale_numerical_features(assigner.NUMERICAL_FEATURES,
-                                                                                   issues_train,
-                                                                                   issues_test)
+                for algorithm, grid_search in get_algorithms():
 
-            for algorithm, grid_search in get_algorithms():
-
-                result = analyse_performance(grid_search, algorithm, issues_train_std, priority_train, issues_test_std,
-                                             priority_test)
-                if result:
-                    results.append(result + (repository, issues_found))
+                    result = analyse_performance(grid_search, algorithm, issues_train_std, priority_train,
+                                                 issues_test_std,
+                                                 priority_test)
+                    if result:
+                        results.append(result + (repository, issues_found))
         else:
             print "Issues corresponding to repository ", repository, " are not enough for analysis."
 
-    file_name = "experiment_results.csv"
-
-    print "Writing results to ", file_name
-    results_dataframe = pd.DataFrame(data=results,
-                                     columns=["Algorithm", "CV Mean", " CV STD",
-                                              "Best configuration",
-                                              "Train accuracy", "Test accuracy", " Test f1-score",
-                                              "Test f1-score Pri 1", "Test f1-score Pri 2", "Test f1-score Pri 3",
-                                              "Test f1-score Pri 4", "Test f1-score Pri 5", "Test Support Pri 1",
-                                              "Test Support Pri 2", "Test Support Pri 3",
-                                              "Test Support Pri 4", "Test Support Pri 5", "Repository", "Total Issues"])
-
-    results_dataframe.to_csv(file_name, index=False)
+    write_results(results)
 
 
 if __name__ == "__main__":
     main()
+    winsound.Beep(2500, 1000)
